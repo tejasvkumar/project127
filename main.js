2dbc2ea6b467a2e570cef8413518abcddf96b94a
tejasvkumar <122033383+tejasvkumar@users.noreply.github.com>
2023-01-28 13:00:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+song1="";
+song2="";
+function preload(){
+    song1=loadSound("song.mp3");
+    song2=loadSound("music.mp3");
+}
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();  
+}
+function draw(){
+    image(video,0,0,600,500);
+}
